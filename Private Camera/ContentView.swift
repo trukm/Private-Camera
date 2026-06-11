b2cd09607b37be0ed40183054fdf3e092fcdf17a
KurtABum <kossie518@gmail.com>
2026-06-11 10:58:22 +1000

    Initial Commit

---
+++ Private Camera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Private Camera
+//
+//  Created by Kurt McCullough on 11/6/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
